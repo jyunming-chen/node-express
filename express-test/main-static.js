af242d1b03ec7b81ccd3900001918a3f3a63d6ad
jyunming-chen <jyunming.chen@gmail.com>
2021-10-31 16:26:00 +0800

    add express-static

---
+++ express-test/main-static.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use (express.static ('public'));
+
+app.get ('/', function (req, res) {
+	console.log ('client connected')
+	res.sendFile (__dirname + "/h1-static.htm");
+});
+
+app.listen (3000, function () {
+	console.log ('server started on port 3000');
+	}) ;
+
